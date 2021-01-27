e93a4af85137efe81ce8ce484335a6ddb55cc598
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-01-27 20:05:36 +0500

    Finished

---
+++ dist/src/Components/Header/Header.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Header = function Header() {
+  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h1", null, "This is a header and it works"));
+};
+
+var _default = Header;
+exports.default = _default;
+
+//# sourceMappingURL=Header.jsx.map
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div>
+           <h1>NavBar</h1>
+           <h2>This is a change made to the NavBar</h2>
+        </div>
+    )
+}
+
+export default NavBar
